54e8c0692ff62ffaa819551f6fd2754533976f03
Li Ruoteng <liruoteng@gmail.com>
2017-01-26 11:41:07 +0800

    remove old matlab files

--- matlab/+eval/nss.m
+++
@@ -1,9 +0,0 @@
-function score = nss(saliencyMap, eyeData)
-    mapMean = mean2(saliencyMap);
-    mapStd = std2(saliencyMap);
-    if mapStd == 0
-        mapStd = 1;
-    end
-    saliencyMap = (saliencyMap - mapMean) / mapStd;
-    score = mean(saliencyMap(eyeData.fixInd));
-end
